a4570e7d18dd02ed5b172af5c2cf06119f4c2a87
mamarquez <mamarquezp@alumno.uspg.edu.gt>
2025-11-08 15:26:16 -0600

    Agregar archivos de proyecto.

---
+++ USPG-PII-Ejercicio_ED/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace USPG_PII_Ejercicio_ED
+{
+    partial class frmEditorTxt
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            btnUndo = new Button();
+            btnSave = new Button();
+            btnRedo = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(588, 321);
+            textBox1.TabIndex = 0;
+            // 
+            // btnUndo
+            // 
+            btnUndo.Location = new Point(12, 360);
+            btnUndo.Name = "btnUndo";
+            btnUndo.Size = new Size(112, 34);
+            btnUndo.TabIndex = 1;
+            btnUndo.Text = "Undo";
+            btnUndo.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(248, 360);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(112, 34);
+            btnSave.TabIndex = 2;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnRedo
+            // 
+            btnRedo.Location = new Point(481, 360);
+            btnRedo.Name = "btnRedo";
+            btnRedo.Size = new Size(112, 34);
+            btnRedo.TabIndex = 3;
+            btnRedo.Text = "Redo";
+            btnRedo.UseVisualStyleBackColor = true;
+            // 
+            // frmEditorTxt
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(605, 417);
+            Controls.Add(btnRedo);
+            Controls.Add(btnSave);
+            Controls.Add(btnUndo);
+            Controls.Add(textBox1);
+            Name = "frmEditorTxt";
+            Text = "Editor de Texto";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button btnUndo;
+        private Button btnSave;
+        private Button btnRedo;
+    }
+}
---
+++ USPG-PII-Ejercicio_ED/Form1.cs
@@ -0,0 +1,10 @@
+namespace USPG_PII_Ejercicio_ED
+{
+    public partial class frmEditorTxt : Form
+    {
+        public frmEditorTxt()
+        {
+            InitializeComponent();
+        }
+    }
+}
